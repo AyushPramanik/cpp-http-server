d402995d4161dedfd7d8d2c8745a0209d5f0a486
Ayush Pramanik <ayushpramanik2007@gmail.com>
2026-02-22 15:04:10 -0500

    tcp/ip setup

--- http_tcpServer_linux.cpp
+++ http_tcpServer_linux.cpp
@@ -1,10 +1,44 @@
 #include <http_tcpServer_linux.h>
 
-namespace http {
-    TcpServer::TcpServer() {
+#include <iostream>
+#include <sstream>
+#include <unistd.h>
 
+namespace {
+    void log(const std::string &message) {
+        std::cout << message << std::endl;
+    }
+    void exitWithError(const std::string &errorMessage) {
+        log("ERROR: " + errorMessage);
+        exit(1);
+    }
+}
+namespace http {
+    TcpServer::TcpServer(std::string ip_address, int port)
+    : m_ip_address(ip_address), m_port(port), m_socket(),
+      m_new_socket(), m_incomingMessage(), m_socketAddress(),
+      m_socketAddress_len(sizeof(m_socketAddress)),
+      m_serverMessage(buildResponse())
+    {
+        startServer();
     }
     TcpServer::~TcpServer() {
+        closeServer();
+    }
 
+    int TcpServer::startServer() {
+        m_socket = socket(AF_INET, SOCK_STREAM, 0);
+        if (m_socket < 0) {
+            exitWithError("Failed to create socket");
+            return 1;
+        }
+
+        return 0;
+    }
+
+    void TcpServer::closeServer() {
+        close(m_socket);
+        close(m_new_socket);
+        exit(0);
     }
 } // namespace http
